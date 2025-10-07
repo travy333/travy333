2d38c9af73cb1ab29dd284ce8090a73b4a548abc
Antonio <capa909@gmail.com>
2025-10-07 19:18:32 +0200

    deploy portfolio site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://travy333.github.io',
-  base: '/',
+  site: 'https://travy333.github.io/travy333',
+  base: '/travy333/',
 });
