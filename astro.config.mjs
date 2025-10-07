ac305c0d4025fc94d4f075b435093874af1bd44e
Antonio <capa909@gmail.com>
2025-10-07 14:28:16 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  // 1. Aggiungi Tailwind tramite la sua integrazione ufficiale
   integrations: [tailwind()],
-
-  // 2. Aggiungi le impostazioni per GitHub Pages
   site: 'https://travy333.github.io',
   base: '/',
 });
